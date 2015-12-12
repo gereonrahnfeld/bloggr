26a64632ed6c53eb3be1836a15df68514c8f39ef
GGR <gereonrahnfeld@gmail.com>
2015-12-12 12:58:47 +0100

    My Django Girls app, first commit

---
+++ atom/first.py
@@ -0,0 +1,12 @@
+def max(number1, number2):
+    if number1 > number2:
+        return(number1)
+    elif number2 > number1:
+        return(number2)
+    else:
+        return(number1)
+
+#print(max(10,1))
+
+b = max(10,11)
+print(b)
